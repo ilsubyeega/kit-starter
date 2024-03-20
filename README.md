27d13e63db832996bd984f0fff694eb6c59a2165
ilsubyeega (Il Sub Shin) <ilsubyeega@outlook.com>
2024-03-20 13:28:12 +0900

    Bump

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Svelte 5 Status
 - [ ] kit: Expanded support for Svelte 5 https://github.com/sveltejs/kit/issues/10761
 - [ ] eslint-plugin-svelte: Svelte 5 runes support https://github.com/sveltejs/eslint-plugin-svelte/issues/587
-
-# Current Issues
-- [ ] storybook: styling does not work as expected
-- [ ] storybook: unable to upgrade svelte5 to latest due to deprecation apis.
---
+++ src/lib/components/button_rune.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+import ButtonRune from './button_rune.svelte';
+
+const meta: Meta<ButtonRune> = {
+	title: 'Components/Button (Rune)',
+	component: ButtonRune,
+	argTypes: {
+		href: { control: 'text' },
+		color: { control: 'color' }
+	}
+};
+export default meta;
+
+type Story = StoryObj<ButtonRune>;
+
+export const Default: Story = {
+	args: {
+		href: 'https://google.com/',
+		color: 'pink'
+	}
+};
+
+export const Primary: Story = {
+    args: {
+        href: 'https://google.com/',
+        color: 'blue'
+    }
+};
